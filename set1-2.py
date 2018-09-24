451f40134a0294a59620cdea26da601f402b10e7
MohamedAshikAnwar <42800432+MohamedAshikAnwar@users.noreply.github.com>
2018-09-24 11:50:58 +0530

    Update set1-2.py

--- set1-2.py
+++ set1-2.py
@@ -1,8 +1,5 @@
 a=int(input("to check even or odd"))
-def numbers(a):
-    if(a%2==0):
-        print('even')
-    else:
-        print('odd')
-
-numbers(a)
+if(a%2==0):
+    print('even')
+else:
+    print('odd')
