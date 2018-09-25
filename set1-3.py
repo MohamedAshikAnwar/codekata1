29ad1e9447fc3e5aa2266ae9feb69f263bc3bd67
MohamedAshikAnwar <42800432+MohamedAshikAnwar@users.noreply.github.com>
2018-09-25 08:59:14 +0530

    Update set1-3.py

--- set1-3.py
+++ set1-3.py
@@ -1,9 +1,6 @@
-z = input();
-if z == '0':
-    exit();
+z=str(input())
+y=['a','e','i','o','u','A','E','I','O','U']
+if(z in y):
+    print('vowel')
 else:
-    if(z=='a' or z=='A' or z=='e' or z=='E' or z=='i' or z=='I'
-       or z=='o' or z=='O' or z=='u' or z=='U'):
-    	    print(z, "vowel");
-    else:
-    	print(z, "consonant");
+    print('consonant')
