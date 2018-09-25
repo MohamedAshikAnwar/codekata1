cf25af4d37b0b35a53cdf262b26c156a13ea2e7e
MohamedAshikAnwar <42800432+MohamedAshikAnwar@users.noreply.github.com>
2018-09-25 09:08:11 +0530

    Update set1-4.py

--- set1-4.py
+++ set1-4.py
@@ -1,5 +1,5 @@
 bc=input()
 if((bc>='a' and bc<= 'z') or (bc>='A' and bc<='Z')):
-    print(bc, "is an Alphabet")
+    print(bc, "Alphabet")
 else:
-    print(bc, "is not an Alphabet")
+    print(bc, "No")
